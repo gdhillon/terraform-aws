649f3e6ed20b590cd7454f955a83081150ae6926
Gurmukh Singh <gurmukh.dhillon@gmail.com>
2023-11-30 10:06:50 +1100

    trying to enable acl on default

--- terraform/modules/bucket/s3.tf
+++ terraform/modules/bucket/s3.tf
@@ -73,7 +73,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
 
 
 resource "aws_s3_bucket_acl" "acl" {
-  count = var.is_acl_on == "true" ? 1 : 0
+  count = "${var.is_acl_on}" == "true" && "${var.environment}" != "dev" ? 1 : 0
   bucket = aws_s3_bucket.bucket.id
   acl    = "private"
 }
